c87d56d75699436bf46b90b19dbc06a2b850f8ee
Akanksha Bij <akankshabij@akankshaMac.local>
2024-06-04 20:17:17 -0400

    Latest HRO updates after Paper draft complete

--- HRO.py
+++ HRO.py
@@ -41,32 +41,6 @@ class HRO:
 
     def HROanalysis(self, convention, msk, hdu, vecMask, gstep, kstep, kernel, histbins, Bproj):
 
-        #self.dMdx_full = self.Map_full.copy()
-        #self.dMdx_full.data = ndimage.filters.gaussian_filter(self.Map_full.data, [kstep, kstep], order=[1,0], mode='nearest')
-        #self.dMdy_full = self.Map_full.copy()
-        #self.dMdy_full.data = ndimage.filters.gaussian_filter(self.Map_full.data, [kstep, kstep], order=[0,1], mode='nearest')
-        
-        # 1. Smooth Map
-        # To avoid convovling NaN's (use two sub-arrays) : https://stackoverflow.com/questions/18697532/gaussian-filtering-a-image-with-nan-in-python
-        
-        # self.dMdx_full = self.Map_full.copy()
-        # V = self.dMdx_full.data.copy()
-        # V[np.isnan(self.dMdx_full.data)]=0
-        # VV_x = ndimage.filters.gaussian_filter(V, [kstep, kstep], order=[1,0], mode='nearest')
-        # W = 0*self.dMdx_full.data.copy() + 1
-        # W[np.isnan(self.dMdx_full.data)]=0
-        # WW_x = ndimage.filters.gaussian_filter(W, [kstep, kstep], order=[0,0], mode='nearest')
-        # self.dMdx_full.data = VV_x / WW_x
-
-        # self.dMdy_full = self.Map_full.copy()
-        # V = self.dMdy_full.data.copy()
-        # V[np.isnan(self.dMdy_full.data)]=0
-        # VV_y = ndimage.filters.gaussian_filter(V, [kstep, kstep], order=[0,1], mode='nearest')
-        # W = 0*self.dMdy_full.data.copy() + 1
-        # W[np.isnan(self.dMdy_full.data)]=0
-        # WW_y = ndimage.filters.gaussian_filter(W, [kstep, kstep], order=[0,0], mode='nearest')
-        # self.dMdy_full.data = VV_y / WW_y
-
         # Qmap is given
         if (self.Qmap is None)==False:
         # #    assert self.Map.shape == self.Qmap.shape and self.Map.shape == self.Umap.shape and self.Qmap.shape == self.Umap.shape, "Dimensions of Map and Polarization data do not match"
@@ -112,15 +86,6 @@ class HRO:
             self.Ex = np.cos(self.Efield) 
             self.Ey = np.sin(self.Efield)
 
-        # Calculate partial spatial derivatives of Map
-        # self.dMdx_, self.dMdy_ = self.Gradient(self.Map, gstep)
-
-        # # Convolve with smoothing Kernel
-        # #self.dMdx = self.dMdx_
-        # #self.dMdy = self.dMdy_
-        # self.dMdx = self.SmoothKernel(self.dMdx_, kstep, type=kernel)
-        # self.dMdy = self.SmoothKernel(self.dMdy_, kstep, type=kernel)
-
         #2. Smooth
         #self.kstep_pix, self.kstep_arcsec = self.KernelLength(self.Map_full, self.Bmap, set_pix=kstep)
         self.dMdx_proj = self.Map_proj.copy()
@@ -133,15 +98,6 @@ class HRO:
 
         # Find Gradient magnitude and direction and contour direction
         self.mag = np.sqrt(self.dMdx**2 + self.dMdy**2)                               # Gradient magnitude
-
-        # NaN out any areas that are close to zero, where gradient turns around:
-
-        # mask gradient amplitude below 10%
-        # percentile = np.linspace(10,100,10)
-        # bins10 = np.nanpercentile(self.mag, percentile)
-        # print(bins10)
-        # self.mag[self.mag<bins10[1]] = np.nan
-
         self.grad = np.arctan2(self.dMdy, self.dMdx)                                  # Gradient direction
         self.contour = np.arctan2(self.dMdx, self.dMdy)                               # Contours direction is perpendicular to gradient vector (defined as angle psi in report)
 
@@ -179,133 +135,21 @@ class HRO:
             self.digitized = np.digitize(self.BinMap, self.sections)
         except:
             self.digitized = np.digitize(self.BinMap, [self.sections[i]*i for i in range(len(self.sections))]) # to avoid monotonically increasing error
-        # print('digitized shape ', digitized.shape)
-        # print(np.min(digitized))
-        # print(np.max(digitized))
         phi_sections = [self.phi[self.digitized == i] for i in range(partitions)]
         self.nvectors = [phi_sect.shape[0] for phi_sect in phi_sections]
-        # print(phi_1.shape)
-        # print(phi_2.shape)
-        # print(phi_3.shape)
         self.hist_array = []; self.bin_edges_array = []
         for phi_sect in phi_sections:
             hist_, bin_edges_ = np.histogram(phi_sect, bins=histbins, range=(0, np.pi/2), density=True)
             self.hist_array.append(hist_)
             self.bin_edges_array.append(bin_edges_)
-        
-        # Split into 3 sections
-        # bins = 3
-        # Mmax = np.nanmax(self.Map)
-        # Mmin = np.nanmin(self.Map)
-        # section = (Mmax - Mmin) / bins 
-        # self.sections = [Mmin, Mmin+section, Mmin+section*2]
-        # print(self.sections)
-        # digitized = np.digitize(self.Map, self.sections)
-        # self.nvectors = [self.phi[digitized==1].shape[0], self.phi[digitized==2].shape[0], self.phi[digitized==3].shape[0]]
-        # phi_1 = self.phi[digitized == 1]
-        # phi_2 = self.phi[digitized == 2]
-        # phi_3 = self.phi[digitized == 3]
-        # self.hist_1, self.bin_edges_1 = np.histogram(phi_1, bins=histbins, range=(0, np.pi/2), density=True)
-        # self.hist_2, self.bin_edges_2 = np.histogram(phi_2, bins=histbins, range=(0, np.pi/2), density=True)
-        # self.hist_3, self.bin_edges_3 = np.histogram(phi_3, bins=histbins, range=(0, np.pi/2), density=True)
-
-    # def CompareVectors2(self, convention, msk, hdu, vecMask, gstep, kstep, kernel, histbins, Bmap2):
-    #     if msk==True:
-    #         for i in range(hdu.data.shape[0]):
-    #             for j in range(hdu.data.shape[1]):
-    #                 if hdu.data[i,j]==0 or vecMask.data[i,j]==0:
-    #                     self.Bmap[i,j]=np.nan
-    #                     self.Bmap2[i,j]=np.nan
-
-    #                                                                 # Standard convention used for most polarization data 
-    #     self.Efield = Bmap + np.pi/2                                # Use 4-quadrant arctan, gives result in radians
-    #     self.Efield2 = Bmap2 + np.pi/2 
-    #     else:
-    #         self.Efield = 0.5*np.arctan2(-1*self.Umap,self.Qmap)
-    #         self.Efield2 = 0.5*np.arctan2(self.Umap2,self.Qmap2)                       # Some observations use the Internantional Astronomical Union convention (e.g Planck)
-
-    #     # Decompose Polarization angle vector into x and y components
-    #     self.Ex = np.cos(self.Efield) 
-    #     self.Ex2 = np.cos(self.Efield2)
-    #     self.Ey = np.sin(self.Efield)
-    #     self.Ey2 = np.sin(self.Efield2)
-
-    #     # Find inferred magnetic field direction
-    #     self.Bfield = self.Efield + np.pi/2                                           # Inferred B-field is 90 deg offset from polarization angle
-    #     self.Bfield2 = self.Efield2 + np.pi/2
-    #     self.Bx = np.cos(self.Bfield) 
-    #     self.Bx2 = np.cos(self.Bfield2) 
-    #     self.By = np.sin(self.Bfield)
-    #     self.By2 = np.sin(self.Bfield2) 
-
-    #     # Calculate Relative angle
-    #     self.dot   = ((self.Bx2)*(self.Bx) + (self.By2)*(self.By))                    # normalized dot product between B-field and contour (note: x and y of gradient flipped for contour direction)
-    #     self.cross = ((self.Bx2)*(self.By) - (self.By2)*(self.Bx))                    # normalized cross product between B-field and contour
-    #     self.phi = np.arctan2(np.abs(self.cross), self.dot)                           # relative angle phi as defined in report, ranges from 0 to 180 deg
-        
-    #     # Caluclate Rayleigh Statistic 
-    #     self.Zx = self.RayleighStatistic(self.phi)
-
-    #     # Wrap phi angle
-    #     self.phi[self.phi > np.pi/2] = np.pi - self.phi[self.phi > np.pi/2]           # wrap angle so now it ranges from 0 to 90 deg (as relative angle phi=85 is the same as phi=95)
-        
-    #     # Make full phi histogram
-    #     self.hist, self.bin_edges = np.histogram(self.phi, bins=histbins, range=(0, np.pi/2), density=True)
-
-    #     #Use percentile to split
-    #     partitions = 5
-    #     percentile = [(100/partitions)*(i+1) for i in range(partitions)]
-    #     self.sections = np.nanpercentile(self.Map, percentile)
-    #     print(self.sections)
-    #     digitized = np.digitize(self.Map, self.sections)
-    #     phi_sections = [self.phi[digitized == i] for i in range(partitions)]
-    #     print(phi_sections[0].shape)
-    #     print(phi_sections[2].shape)
-    #     self.nvectors = [phi_sect.shape[0] for phi_sect in phi_sections]
-    #     self.hist_array = []; self.bin_edges_array = []
-    #     for phi_sect in phi_sections:
-    #         hist_, bin_edges_ = np.histogram(phi_sect, bins=histbins, range=(0, np.pi/2), density=True)
-    #         self.hist_array.append(hist_)
-    #         self.bin_edges_array.append(bin_edges_)
+    
         
     def CompareVectors(self, convention, msk, hdu, vecMask, gstep, kstep, kernel, histbins, Qmap2, Umap2):
-        # Band E
+        ''' This function calculates the relative angles between magnetic field maps (i.e BLASTPol vs. HAWC'''    
         self.Qmap2 = projectMap(Qmap2, self.Qmap).data
         self.Umap2 = projectMap(Umap2, self.Umap).data
         self.Qmap = self.Qmap.data
         self.Umap = self.Umap.data
-
-        #Smooth Band C to resolution of Band E
-        #size = self.getSmoothingSize(old=7.8, new=18.2)                   # FWHM sizes of Bend C and Band E from : https://www.sofia.usra.edu/science/proposing-and-observing/sofia-observers-handbook-cycle-6/8-hawc/81-specifications#Table%208-1
-        #size = self.getSmoothingSize(old=4.02, new=9.37)
-        #print('size is ', size)
-        #size=4
-        #self.Qmap = self.SmoothKernel(data=self.Qmap, size=size, type='Gaussian')
-        #self.Umap = self.SmoothKernel(data=self.Umap, size=size, type='Gaussian')
-
-        # fig = plt.figure(figsize=[12,8])
-        # fxx = aplpy.FITSFigure(self.Umap, figure=fig)
-        # fxx.show_colorscale(vmax=0.2, vmin=-0.2)
-        # fxx.add_colorbar()
-        # plt.savefig('/Users/akankshabij/Documents/MSc/Research/Code/scripts/HRO/HRO_BijA/Output_Plots/BandC/FieldComparison/Smoothed/Umap_BandC.png')
-
-        # fig = plt.figure(figsize=[12,8])
-        # fxx = aplpy.FITSFigure(self.Qmap, figure=fig)
-        # fxx.show_colorscale(vmax=0.2, vmin=-0.2)
-        # fxx.add_colorbar()
-        # plt.savefig('/Users/akankshabij/Documents/MSc/Research/Code/scripts/HRO/HRO_BijA/Output_Plots/BandC/FieldComparison/Smoothed/Qmap_BandC.png')
-
-        # fig = plt.figure(figsize=[12,8])
-        # fxx = aplpy.FITSFigure(self.Qmap2, figure=fig)
-        # fxx.show_colorscale(vmax=0.2, vmin=-0.2)
-        # fxx.add_colorbar()
-        # plt.savefig('/Users/akankshabij/Documents/MSc/Research/Code/scripts/HRO/HRO_BijA/Output_Plots/BandC/FieldComparison/Smoothed/Qmap_BandE.png')
-
-        # fig = plt.figure(figsize=[12,8])
-        # fxx = aplpy.FITSFigure(self.Umap2, figure=fig)
-        # fxx.show_colorscale(vmax=0.2, vmin=-0.2)
-        # fxx.add_colorbar()
-        # plt.savefig('/Users/akankshabij/Documents/MSc/Research/Code/scripts/HRO/HRO_BijA/Output_Plots/BandC/FieldComparison/Smoothed/Umap_BandE.png')
 
         if convention==1:                                                             # Standard convention used for most polarization data 
             self.Efield = 0.5*np.arctan2(self.Umap,self.Qmap)                         # Use 4-quadrant arctan, gives result in radians
